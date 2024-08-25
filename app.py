d9ebcc6f15f09f9abcd6886b6326eddad460b6cc
Ali Amgad <mostafaamgadaboseif@gmail.com>
2024-08-25 20:40:49 +0300

    Changed navigation in Html files

--- app.py
+++ app.py
@@ -11,7 +11,8 @@ from markupsafe import escape
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'static/uploads'
 connection = db.connect_to_database()
-app.secret_key = "SUPER-SECRET"
+app.secret_key = "3abas"
+#app.secret_key = os.environ.get('SECRET_KEY', 'default_secret_key')
 limiter = Limiter(app=app, key_func=get_remote_address, default_limits=["50 per minute"], storage_uri="memory://")
 app.config['SESSION_COOKIE_HTTPONLY'] = False
 
@@ -72,7 +73,68 @@ def setting():
             return render_template('setting.html', data=data) 
     else:
         return redirect(url_for('login'))
+    
+@app.route('/')   
+def index():
+    if 'username' in session:
+        #if session['username'] == 'admin':
+            #return list(db.get_all_users(connection))
+        #else:
+            return render_template('index.html')
+    return redirect(url_for('login'))
 
+
+@app.route('/login', methods=['GET', 'POST'])
+@limiter.limit("10 per minute")
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        user = db.get_user(connection, username)
+
+        if user:
+            if utils.is_password_match(password, user[2]):
+                session['username'] = user[1]
+                return redirect(url_for('index'))
+            else:
+                flash("Password dose not match", "danger")
+                return render_template('login.html')
+
+        else:
+            flash("Invalid username", "danger")
+            return render_template('login.html')
+
+    return render_template('login.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+@limiter.limit("10 per minute")
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if not utils.is_strong_password(password):
+            flash(
+                "Sorry You Entered a weak Password Please Choose a stronger one", "danger")
+            return render_template('register.html')
+
+        user = db.get_user(connection, username)
+        if user:
+            flash(
+                "Username already exists. Please choose a different username.", "danger")
+            return render_template('register.html')
+        else:
+            db.add_user(connection, username, password)
+            return redirect(url_for('login'))
+
+    return render_template('register.html')
+
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('index'))
 
 #-------------------------------------------------------------------
 if __name__ == '__main__':
